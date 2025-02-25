ed984ccd99491d81d238fb54b0306f0ddad3992b
Papu Sethi <mrpapur2001@gmail.com>
2025-02-25 23:21:54 +0530

    feat: Completed helmet setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import helmet from 'helmet';
 import path from 'path';
+import responseMessage from './constant/responseMessage';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import router from './router/apiRouter';
 import httpError from './utils/httpError';
-import responseMessage from './constant/responseMessage';
 
 const app = express();
 
 // Middlewares
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
