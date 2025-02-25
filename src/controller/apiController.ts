542f386dc0061dfe272ed023764e6f0ac3009eab
Papu Sethi <mrpapur2001@gmail.com>
2025-02-26 00:23:36 +0530

    fix: Updated script and formatted code

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import httpResponse from '../utils/httpResponse';
 import responseMessage from '../constant/responseMessage';
 import httpError from '../utils/httpError';
+import httpResponse from '../utils/httpResponse';
 import quicker from '../utils/quicker';
 
 export default {
