97b71a3318a933e01bc175748ef185c9ee5d7c36
Papu Sethi <mrpapur2001@gmail.com>
2025-02-25 23:44:42 +0530

    feat: Completed rate limiter setup

--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import apiController from '../controller/apiController';
+// import rateLimit from '../middleware/rateLimit';
 
 const router = Router();
 
+// router.use(rateLimit); // This rate limiter will be applied for all the endpoints.
 router.route('/self').get(apiController.self);
 router.route('/health').get(apiController.health);
 
